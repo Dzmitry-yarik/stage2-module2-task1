7cbae9a2ecb7cc4857067ae7ba9d0d93db962967
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-04-27 19:24:11 +0300

    Update GetUsersServlet.java

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,37 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+import java.io.IOException;
+import java.util.Set;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+    private final static String index = "jsp/users.jsp";
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Set<User> users = Warehouse.getInstance().getUsers();
+        req.setAttribute("users",users);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher(index);
+       try {
+           requestDispatcher.forward(req, resp);
+       }catch (ServletException | IOException e){
+           e.printStackTrace();
+       }
+    }
+
 }
